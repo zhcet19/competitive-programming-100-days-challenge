f562a28b5df1e499e408a160c7f8d7e7811bebec
Faiz alam <55043876+zhcet19@users.noreply.github.com>
2020-05-29 19:23:13 +0530

    Add files via upload

---
+++ new.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+int main()
+{
+    int num ;
+    std::cout<<"enter the no to convert from decimal to binary:";
+    std::cin>>num;
+    int a[10];
+    int i=0;
+    for(i=0;num>0;i++)
+    {
+        a[i]= num%2;
+
+        num=num/2;
+    }
+
+    std::cout<<"the binary output is";
+    for(i=i-1;i>=0;i--)
+    {
+       std::cout<<a[i];
+    }
+  return 0;
+
+}
