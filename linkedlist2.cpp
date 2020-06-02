544396f55aa45d5f337b91273b400deb3b8a1b8d
Faiz alam <55043876+zhcet19@users.noreply.github.com>
2020-06-02 22:17:04 +0530

    Adding a node to a  singly linked list from end

---
+++ linkedlist2.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+using namespace std;
+struct Node{
+    int data;
+    Node* next;
+};
+Node* head;
+void insert(int x)
+{ Node* temp=new Node();
+  temp->data=x;
+  temp->next=NULL;
+  if(head==NULL){
+    head=temp;
+  }
+  else{
+    Node* temp1=head;
+while(temp1->next!=NULL)     {temp1=temp1->next;}
+
+    temp1->next=temp;
+  }
+  return;
+
+}
+
+void print(){
+    Node* temp=head;
+    std::cout<<"list is:";
+    while(temp!=NULL)
+    {
+        std::cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+    std::cout<<"\n";
+}
+int main()
+{
+    head=NULL;
+    int n,x;
+    std::cout<<"How many no you want to insert in this linked list\n";
+    std::cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        std::cout<<"Enter the no\n";
+        std::cin>>x;
+        insert(x);
+        print();
+    }
+}
+
+
+
